f3aacdf2b6ef4fb9ff447cbebcb0ee23b45161e4
Sampathkumar Danesh Karani <sampathkarani@gmail.com>
2026-04-05 22:33:03 +0530

    1st program

---
+++ Hello.java
@@ -0,0 +1,17 @@
+
+ 
+class Hello
+{ 
+    public static void main(String args[])
+    {
+         int num1 = 7;
+         num1=num1+2;
+         int num2 = 5;
+         int result =num1+num2;
+         num1 += 3;
+         num2++;
+         System.out.println(num1);
+         System.out.println(num2);
+         System.out.println(result);
+    }
+}
