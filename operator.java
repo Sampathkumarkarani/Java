164a985968753ae5924911a4977128568ab6f1ed
Sampathkumar Danesh Karani <sampathkarani@gmail.com>
2026-04-05 22:44:22 +0530

    Program using Operator

---
+++ operator.java
@@ -0,0 +1,13 @@
+public class operator
+{
+    public static void main(String args[])
+    {
+        double x = 8.6;
+        double y = 9.6;
+
+               
+
+        boolean result = x <= y;
+        System.out.println(result);
+    }
+}
