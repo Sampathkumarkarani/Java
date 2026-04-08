83cc122e2f9e016322840b9cd88abeb1b65532fa
Sampathkumar Danesh Karani <sampathkarani@gmail.com>
2026-04-09 00:33:34 +0530

    Switch Case

---
+++ switch1.java
@@ -0,0 +1,36 @@
+public class switch1 
+{
+    public static void main(String args[])
+    {
+        int n=5;
+        
+        switch(n){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:    
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("wednesday");
+                break;
+            case 4:
+                System.out.println("thursday");
+                break;
+            case 5:    
+                System.out.println("friday");
+                break;
+            case 6:    
+                System.out.println("saturday");
+                break;
+            case 7:
+                System.out.println("sunday");
+                break;
+            default:
+                System.out.println("Enter a valid number");
+             
+        }   
+        
+          
+    }
+}
